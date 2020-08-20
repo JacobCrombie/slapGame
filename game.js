148b4ddb7819db5f38797ff8809925799841b20f
Darbyblinko <jacobcrombie@live.com>
2020-08-20 14:38:50 -0600

    adams turn

--- game.js
+++ game.js
@@ -1,12 +1,30 @@
 let health = 100
+let hits = 0
 
+let moves ={
+  slap: 1,
+  kick: 10,
+  punch: 5
+}
 
-
-
+function drawStats(){
+document.getElementById("health").innerText = health.toString()
+document.getElementById("hits").innerText = hits.toString()
+}
 
 
 function attack(move){
-  health--
-  console.log("health")
+hits++
+
+  if(health > 0){
+    health-= moves[move]
+  }else{
+    health = 100
+    hits = 0
+    console.log("dead")
+  }
+  drawStats()
 }
 
+
+drawStats()
